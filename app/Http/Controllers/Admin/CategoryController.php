f72b7bb423d28cb22e537bd44fdee931cbc12851
LuqmanSyah <eno.luqman@gmail.com>
2023-09-06 11:19:40 +0700

    Crud category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -14,7 +14,13 @@ class CategoryController extends Controller
     {
         $categories = Category::latest()->get();
 
-        return view('pages.admin.category.index', compact('categories'));
+        $category = [];
+        if (request('id')) {
+            $category = Category::find(request('id'));
+        }
+
+
+        return view('pages.admin.category.index', compact('categories', 'category'));
     }
 
     public function store(Request $request)
@@ -30,6 +36,34 @@ class CategoryController extends Controller
         Category::create($validatedData);
         
         Alert::success('Berhasil!', 'Data kategori berhasil ditambahkan!');
+
+        return back();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'unique:categories,name,' . $id],
+        ]);
+
+        $validatedData['slug'] = Str::slug($request->name);
+        $validatedData['description'] = $request->description;
+        $validatedData['type'] = $request->type;
+
+        $category->update($validatedData);
+        
+        Alert::success('Berhasil!', 'Data kategori berhasil diubah!');
+
+        return redirect()->route('admin.category.index');
+    }
+
+    public function delete($id)
+    {
+        Category::find($id)->delete();
+
+        Alert::success('Berhasil!', 'Data kategori berhasil dihapus!');
 
         return back();
     }
--- resources/views/pages/admin/category/index.blade.php
+++ resources/views/pages/admin/category/index.blade.php
@@ -7,27 +7,61 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                    <form action="{{ route('admin.category.store') }}" method="POST">
+                    <form
+                        action="{{ !request('id') ? route('admin.category.store') : route('admin.category.update', $category->id) }}"
+                        method="POST">
                         @csrf
+                        @if (request('id'))
+                            @method('put')
+                        @endif
                         <div class="row">
                             <div class="form-group mb-3">
                                 <label for="name" class="form-label">Nama</label>
-                                <input type="text" name="name" id="name" class="form-control">
+                                <input type="text" name="name" id="name"
+                                    class="form-control @error('name') is-invalid @enderror"
+                                    value="{{ $category ? $category->name : null }}">
+                                @error('name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="form-group mb-3">
                                 <label for="description" class="form-label">Deskripsi</label>
-                                <input type="text" name="description" id="description" class="form-control">
+                                <input type="text" name="description" id="description"
+                                    class="form-control @error('description') is-invalid @enderror"
+                                    value="{{ $category ? $category->description : null }}">
+                                @error('description')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="form-group mb-3">
                                 <label for="type" class="form-label">Tipe</label>
-                                <select name="type" id="type" class="form-control">
+                                <select name="type" id="type"
+                                    class="form-control @error('type') is-invalid @enderror">
+                                    @if (request('id'))
+                                        <option hidden value="{{ $category->type }}">
+                                            @if ($category->type == 'news')
+                                                Berita
+                                            @elseif($category->type == 'post')
+                                                Forum
+                                            @elseif($category->type == 'general')
+                                                Umum
+                                            @endif
+                                        </option>
+                                    @endif
                                     <option value="general">Umum</option>
                                     <option value="post">Forum</option>
                                     <option value="news">Berita</option>
                                 </select>
+                                @error('type')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-success float-end">Tambah Kategori</button>
+                        @if (request('id'))
+                            <button type="submit" class="btn btn-success float-end">Simpan</button>
+                        @else
+                            <button type="submit" class="btn btn-success float-end">Tambah Kategori</button>
+                        @endif
                     </form>
                 </div>
             </div>
@@ -61,13 +95,21 @@
                                             @if ($category->type == 'news')
                                                 Berita
                                             @elseif($category->type == 'post')
-                                                Postingan
+                                                Forum
                                             @elseif($category->type == 'general')
                                                 Umum
                                             @endif
                                         </td>
                                         <td>
-                                            <a href="" class="btn btn-danger">Hapus</a>
+                                            <div class="d-flex gap-2">
+                                                <a href="?id={{ $category->id }}" class="btn btn-warning btn-sm">Edit</a>
+                                                <form action="{{ route('admin.category.delete', $category->id) }}"
+                                                    method="POST">
+                                                    @method('delete')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @endforeach
